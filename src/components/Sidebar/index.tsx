70c815e260a26dd731131b967b50170027a496b1
anggagumilang212 <anggagumilang212@gmail.com>
2024-05-21 08:13:54 +0700

    gatau

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -404,6 +404,16 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                               Barang Distributor
                             </Link>
                           </li>
+                          <li>
+                            <Link
+                              href="/barangdistributor/transaksi"
+                              className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${pathname === "/barangdistributor/transaksi" &&
+                                "text-white"
+                                }`}
+                            >
+                              Transaksi Distributor
+                            </Link>
+                          </li>
 
 
                         </ul>
